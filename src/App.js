d04a689d542c0e1b183cc099bfada334025fe6dc
e heineman <eric.h.heineman@gmail.com>
2021-11-22 13:50:51 -0500

    Add Header, Button, Tasks, and Task components, styling from tutorial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-function App() {
+import { useState } from 'react';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30 PM',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30 PM',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30 PM',
+        reminder: false,
+    }
+  ])
+
   return (
-    <div className="App">
-      <h1>Hello from React</h1>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
